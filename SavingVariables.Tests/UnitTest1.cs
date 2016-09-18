1b684d1ea0c4637cc2c53ffcdd97a6160d8c0381
Scott Schaffer <scottpschaffer@hotmail.com>
2016-09-17 19:12:30 -0700

    finished Regexes

--- SavingVariables.Tests/UnitTest1.cs
+++ SavingVariables.Tests/UnitTest1.cs
@@ -13,20 +13,18 @@ namespace SavingVariables.Tests
             string test1 = "q";
             string[] actResults = { "q", "error", "error" };
             string[] expResults = e1.Extract(test1);
-            Assert.AreEqual(expResults[0], actResults[0]);
-            //Assert.AreEqual(expResults[1], actResults[1]);
-            //Assert.AreEqual(expResults[2], actResults[2]);
+            CollectionAssert.AreEqual(expResults, actResults);
         }
 
-        [TestMethod]
-        public void Test_Regex_r2_output_valid_0()
-        {
-            Expression e2 = new Expression();
-            string test2 = " r = 3";
-            string[] actResults = { "r", "=", "3" };
-            string[] expResults = e2.Extract(test2);
-            Assert.AreEqual(expResults[0], actResults[0]);
-        }
+        //[TestMethod]
+        //public void Test_Regex_r1_output_invalid()
+        //{
+        //    Expression e1 = new Expression();
+        //    string test1 = "qq";
+        //    string[] actResults = { "error", "error", "error" };
+        //    string[] expResults = e1.Extract(test1);
+        //    CollectionAssert.AreEqual(expResults, actResults);
+        //}
 
         [TestMethod]
         public void Test_Regex_r2_output_valid_1()
@@ -35,17 +33,57 @@ namespace SavingVariables.Tests
             string test3 = " r = 3";
             string[] actResults = { "r", "=", "3" };
             string[] expResults = e3.Extract(test3);
-            Assert.AreEqual(expResults[1], actResults[1]);
+            CollectionAssert.AreEqual(expResults, actResults);
         }
 
         [TestMethod]
         public void Test_Regex_r2_output_valid_2()
         {
             Expression e4 = new Expression();
-            string test4 = " r = 3";
-            string[] actResults = { "r", "=", "3" };
+            string test4 = " r = 34567";
+            string[] actResults = { "r", "=", "34567" };
             string[] expResults = e4.Extract(test4);
-            Assert.AreEqual(expResults[2], actResults[2]);
+            CollectionAssert.AreEqual(expResults, actResults);
+        }
+
+        [TestMethod]
+        public void Test_Regex_r2_output_valid_3()
+        {
+            Expression e4 = new Expression();
+            string test4 = " r = -32";
+            string[] actResults = { "r", "=", "-32" };
+            string[] expResults = e4.Extract(test4);
+            CollectionAssert.AreEqual(expResults, actResults);
+        }
+
+        [TestMethod]
+        public void Test_Regex_r3_output_valid_1()
+        {
+            Expression e5 = new Expression();
+            string test5 = "lastq";
+            string[] actResults = { "lastq", "error", "error" };
+            string[] expResults = e5.Extract(test5);
+            CollectionAssert.AreEqual(expResults, actResults);
+        }
+
+        [TestMethod]
+        public void Test_Regex_r4_output_valid_1()
+        {
+            Expression e6 = new Expression();
+            string test6 = "clear a";
+            string[] actResults = { "clear", "a", "error" };
+            string[] expResults = e6.Extract(test6);
+            CollectionAssert.AreEqual(expResults, actResults);
+        }
+
+        [TestMethod]
+        public void Test_Regex_r4_output_valid_2()
+        {
+            Expression e7 = new Expression();
+            string test7 = "remove all";
+            string[] actResults = { "remove", "all", "error" };
+            string[] expResults = e7.Extract(test7);
+            CollectionAssert.AreEqual(expResults, actResults);
         }
     }
 }
--- SavingVariables/Expression.cs
+++ SavingVariables/Expression.cs
@@ -18,13 +18,21 @@ namespace SavingVariables
             string[] expr1 = { "error", "error", "error" };
 
             // If just letter, then print value
-            Regex r1 = new Regex(@"\s*(?<variable>^[a-z])\s*");
+            Regex r1 = new Regex(@"\s*(?<variable>^[a-z]$)\s*");
 
             // If "letter = number", then variable assigned value
             Regex r2 = new Regex(@"\s*(?<variable>[a-z])\s*=\s*(?<value>[-]?\d+)\s*");
-            
+
+            // If text is one word command
+            Regex r3 = new Regex(@"\s*(^(?<command>[a-z]+)$)\s*");
+
+            // If Text is two words separated by a space
+            Regex r4 = new Regex(@"\s*(^(?<command>[a-z]+)\s?(?<value>[a-z]+)$)\s*");
+
             Match m1 = r1.Match(expr);
             Match m2 = r2.Match(expr);
+            Match m3 = r3.Match(expr);
+            Match m4 = r4.Match(expr);
 
             if (m1.Success)
             {
@@ -37,6 +45,17 @@ namespace SavingVariables
                 expr1[0] = g2["variable"].Value;
                 expr1[1] = "=";
                 expr1[2] = g2["value"].Value;
+            }
+            else if (m3.Success)
+            {
+                GroupCollection g3 = m3.Groups;
+                expr1[0] = g3["command"].Value;
+            }
+            else if (m4.Success)
+            {
+                GroupCollection g4 = m4.Groups;
+                expr1[0] = g4["command"].Value;
+                expr1[1] = g4["value"].Value;
             }
 
             return expr1;
